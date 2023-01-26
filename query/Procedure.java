999428d07df950593677c4c2bff05735eb3fc187
taemin <myloopmy@gmail.com>
2023-01-26 23:58:29 +0900

    프로시져 예제 코드

--- query/Procedure.java
+++ query/Procedure.java
@@ -5,5 +5,24 @@ public class Procedure {
 
     // 예제 시작
 
+//    # PROCEDURE 프로시저의 기본 문법
 
+//    CREATE [OR REPLACE] procedure name
+//    IN argument
+//    OUT argument
+//    IN OUT argument
+//
+//            IS
+//
+//   [변수의 선언]
+//
+//    BEGIN  --> 필수
+//
+//   [PL/SQL Block]
+//            -- SQL문장, PL/SQL제어 문장
+//
+//   [EXCEPTION]  --> 선택
+//  -- error가 발생할 때 수행하는 문장
+//
+//            END;  --> 필수
 }
