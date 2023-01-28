6f1bcc3925e087a27ad50519152d1b637756c48f
taemin <myloopmy@gmail.com>
2023-01-28 23:10:42 +0900

    SQL 순서

--- query/DB_SQL_2.java
+++ query/DB_SQL_2.java
@@ -2,6 +2,45 @@ package query;
 
 public class DB_SQL_2 {
     public static void main(String[] args) {
-        `
+//        SQL의 문법 순서와 실행 작동 순서는 서로 다르다.
+//
+//
+//
+//
+//
+//        문법 작성 순서
+//
+//        SELECT 컬럼명
+//        FROM 테이블명
+//        WHERE 조건식
+//        GROUP BY 컬럼명
+//        HAVING 조건식
+//        ORDER BY 컬럼명
+//
+//        실행 작동 순서
+//
+//                FROM
+//        ON
+//                JOIN
+//        WHERE
+//        GROUP BY
+//        CUBE | ROLLUP
+//        HAVING
+//                SELECT
+//        DISTINCT
+//        ORDER BY
+//        TOP
+//
+//
+//        자주 사용하는 노란 글씨의 쿼리문의 간략한 설명은 다음과 같다.
+//
+//                FROM - 조회 테이블 확인
+//        WHERE - 데이터 추출 조건 확인
+//        GROUP BY - 컬럼 그룹화
+//        HAVING - 그룹화 조건
+//        SELECT - 데이터 추출
+//        ORDER BY - 데이터 순서 정렬
+//
+
     }
 }
